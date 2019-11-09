d0a201aaf5f5f15f8f6c27c47903d5a9e5cd79f2
Francisca <frangutnei@gmail.com>
2019-11-09 11:10:42 -0300

    final

--- themes/mi_tema/header.php
+++ themes/mi_tema/header.php
@@ -8,5 +8,18 @@
 	<?php wp_head() ?>
 </head>
 <body>
+	<nav class="navbar navbar-expand-lg navigation navbar-light" role="navigation">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<a class="navigation__logo" href="<?php bloginfo('url') ?>"><img src="<?php echo get_theme_file_uri(); ?>/assets/images/logo_solo.png" class="navigation__logo--img" alt=""> UpDance</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navigation__span"><i class="fas fa-bars navigation__hamburguer"></i></span>
+		</button>
 
-	<?php get_template_part('_includes/nav', 'bootstrap4') ?>
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		<?php get_template_part('_includes/nav', 'bootstrap4') ?>
+		
+			<li class="navigation__item">
+				<a class="navigation__link--login" href="login">Login</a>
+			</li>
+		</div>
+	</nav>
